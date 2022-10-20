cac684660faba4c11f27c5411f2ce9d00f1c4891
Khaled,El-Sayed <t0ti20@icloud.com>
2022-10-20 07:17:31 +0200

    New On And Off Time

--- Project/COTS/APP/Main.c
+++ Project/COTS/APP/Main.c
@@ -13,24 +13,46 @@
 ----------    GLOBAL DATA     ------------
 *****************************************/
 /********************************************************************
-* Syntax          : int main()
+* Syntax          : int main(void)
 * Description     : Main Function
 ********************************************************************/
-int main()
+int main(void)
 {
+	Global_u32On_Time_ms=10000;
+	Global_u32Off_Time_ms=1000;
 	System_Initialization();
 	GPT_VidTimer_0_A_ISR(Timer_ISR);
 	while(TRUE){}//Busy_Wait
 }
+/********************************************************************
+* Syntax          : void System_Initialization(void)
+* Description     : System Initialization Function
+********************************************************************/
 void System_Initialization(void)
 {
 	RCC_VidClock_Enable(RCC_GPIO,Port_A,Enable);
 	RCC_VidClock_Enable(RCC_GPT,Timer_0,Enable);
 	GPIO_VidMode_Pin(Port_A,Pin_0,GPIO_Output);
-	GPT_VidTimer_0_Delay_ms(Delay_Time_ms,GPT_Interrupt_Enable);
+	GPT_VidTimer_0_Delay_ms(Global_u32Off_Time_ms,GPT_Interrupt_Enable);
 	NVIC_VidInterrupt_Peripheral_Set(GPT_0_A,NVIC_Enable,NVIC_GROUP_0,NVIC_SUP_GROUP_0);
 }
+/********************************************************************
+* Syntax          : void Timer_ISR(void)
+* Description     : Timer Interrupt Service routine
+********************************************************************/
 void Timer_ISR(void)
 {
-	GPIO_VidPin_Toggle(Port_A,Pin_0);
+	switch(Global_u8Initial_State)
+	{
+		case(Led_Off):
+			Global_u8Initial_State=Led_On;
+			GPIO_VidPin_Set(Port_A,Pin_0,Hi);
+			GPT_VidTimer_0_Delay_ms(Global_u32On_Time_ms,GPT_Interrupt_Enable);
+		break;
+		case(Led_On):
+			Global_u8Initial_State=Led_Off;
+			GPIO_VidPin_Set(Port_A,Pin_0,Low);
+			GPT_VidTimer_0_Delay_ms(Global_u32Off_Time_ms,GPT_Interrupt_Enable);
+		break;
+	}
 }
--- Project/COTS/APP/Main.h
+++ Project/COTS/APP/Main.h
@@ -20,9 +20,16 @@
 void Timer_ISR(void);
 void System_Initialization(void);
 /*****************************************
------ GLOBAL DATA TYPES & STRUCTURES -----
+--------------   MACROS   ----------------
 *****************************************/
 #define Delay_Time_ms									(10000)
+#define Led_On												(1)
+#define Led_Off												(0)
+/*****************************************
+--------------- GLOBAL DATA  -------------
+*****************************************/
+static u32 Global_u32On_Time_ms,Global_u32Off_Time_ms;
+static u8 Global_u8Initial_State=Led_Off;
 #endif
 /********************************************************************
  *  END OF FILE: Main.h
